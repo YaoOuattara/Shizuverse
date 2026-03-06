1dcba0909c76e639752357dd1fc137e664e69f69
Yao Ouattara <ouattaranourdine.ym@gmail.com>
2026-03-06 02:03:17 -0500

    Fix password_hash field length in User model

--- shizuverse/models/user.py
+++ shizuverse/models/user.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     user_type = db.Column(db.String(20), nullable=False)  # 'client' or 'provider'
     preferred_language = db.Column(db.String(10), default='fr')
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
