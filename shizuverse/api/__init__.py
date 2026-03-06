854d60ac6280802689dfbfc172ed5b083001e212
Yao Ouattara <ouattaranourdine.ym@gmail.com>
2026-03-06 06:43:39 -0500

    Register appointments blueprint in api

--- shizuverse/api/__init__.py
+++ shizuverse/api/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
 from .services import services_bp
+from .appointments.appointments import appointments_bp
 
 api_bp = Blueprint("api", __name__)
 api_bp.register_blueprint(services_bp, url_prefix="/services")
+api_bp.register_blueprint(appointments_bp, url_prefix="/appointments")
 
 __all__ = ["api_bp"]
-
--- shizuverse/api/appointments/__init__.py
+++ shizuverse/api/appointments/__init__.py
@@ -1,15 +1,1 @@
-import os
-from flask import Blueprint
-from flasgger.utils import swag_from
-from .controller import get_appointments
-
-appointments_bp = Blueprint('appointments', __name__)
-
-@appointments_bp.route('/', methods=['GET'])
-def index():
-    return {"message": "Appointments service is live"}
-
-@appointments_bp.route('/all', methods=['GET'])
-@swag_from(os.path.join(os.path.dirname(__file__), '../../docs/appointments/all.yaml'))
-def all():
-    return get_appointments()
+# Appointments package - blueprint defined in appointments.py
